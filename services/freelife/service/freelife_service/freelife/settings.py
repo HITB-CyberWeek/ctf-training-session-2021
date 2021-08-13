f8fa290f78cbb5bc5e2681c38141706abfa2180a
Andrew Gein <andgein@yandex.ru>
2021-08-13 08:33:03 +0300

    [freelife] Mount database for persistancy

--- services/freelife/service/freelife_service/freelife/settings.py
+++ services/freelife/service/freelife_service/freelife/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'freelife.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
